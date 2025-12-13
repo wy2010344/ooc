3138660507f98f502364ffd702c22fa2e99dc5a4
wy2010344 <wangyang2010344@foxmail.com>
2025-12-13 17:07:39 +0800

    add language basic rule

--- packages/language/test/linking.test.ts
+++ packages/language/test/linking.test.ts
@@ -1,53 +1,54 @@
-import { afterEach, beforeAll, describe, expect, test } from "vitest";
-import { EmptyFileSystem, type LangiumDocument } from "langium";
-import { expandToString as s } from "langium/generate";
-import { clearDocuments, parseHelper } from "langium/test";
-import type { OOCModel } from "object-oriented-c-language";
-import { createObjectOrientedCServices, isOOCModel } from "object-oriented-c-language";
+import { afterEach, beforeAll, describe, expect, test } from 'vitest'
+import { EmptyFileSystem, type LangiumDocument } from 'langium'
+import { expandToString as s } from 'langium/generate'
+import { clearDocuments, parseHelper } from 'langium/test'
+import type { OOCModel } from 'object-oriented-c-language'
+import {
+  createObjectOrientedCServices,
+  isOOCModel,
+} from 'object-oriented-c-language'
 
-let services: ReturnType<typeof createObjectOrientedCServices>;
-let parse:    ReturnType<typeof parseHelper<OOCModel>>;
-let document: LangiumDocument<OOCModel> | undefined;
+let services: ReturnType<typeof createObjectOrientedCServices>
+let parse: ReturnType<typeof parseHelper<OOCModel>>
+let document: LangiumDocument<OOCModel> | undefined
 
 beforeAll(async () => {
-    services = createObjectOrientedCServices(EmptyFileSystem);
-    parse = parseHelper<OOCModel>(services.ObjectOrientedC);
-
-    // activate the following if your linking test requires elements from a built-in library, for example
-    // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
-});
+  services = createObjectOrientedCServices(EmptyFileSystem)
+  parse = parseHelper<OOCModel>(services.ObjectOrientedC)
+})
 
 afterEach(async () => {
-    document && clearDocuments(services.shared, [ document ]);
-});
+  document && clearDocuments(services.shared, [document])
+})
 
 describe('Linking tests', () => {
+  test('basic binding and message passing', async () => {
+    document = await parse(`
+            a = {
+                (hello name) {
+                    name
+                }
+            };
+            b = a hello 'world'
+        `)
 
-    test('linking of greetings', async () => {
-        document = await parse(`
-            person Langium
-            Hello Langium!
-        `);
-
-        expect(
-            // here we first check for validity of the parsed document object by means of the reusable function
-            //  'checkDocumentValid()' to sort out (critical) typos first,
-            // and then evaluate the cross references we're interested in by checking
-            //  the referenced AST element as well as for a potential error message;
-            checkDocumentValid(document)
-                || document.parseResult.value.greetings.map(g => g.person.ref?.name || g.person.error?.message).join('\n')
-        ).toBe(s`
-            Langium
-        `);
-    });
-});
+    expect(checkDocumentValid(document)).toBeUndefined()
+  })
+})
 
 function checkDocumentValid(document: LangiumDocument): string | undefined {
-    return document.parseResult.parserErrors.length && s`
+  return (
+    (document.parseResult.parserErrors.length &&
+      s`
         Parser errors:
-          ${document.parseResult.parserErrors.map(e => e.message).join('\n  ')}
-    `
-        || document.parseResult.value === undefined && `ParseResult is 'undefined'.`
-        || !isOOCModel(document.parseResult.value) && `Root AST object is a ${document.parseResult.value.$type}, expected a 'OOCModel'.`
-        || undefined;
+          ${document.parseResult.parserErrors
+            .map((e) => e.message)
+            .join('\n  ')}
+    `) ||
+    (document.parseResult.value === undefined &&
+      `ParseResult is 'undefined'.`) ||
+    (!isOOCModel(document.parseResult.value) &&
+      `Root AST object is a ${document.parseResult.value.$type}, expected a 'OOCModel'.`) ||
+    undefined
+  )
 }
--- packages/language/test/parsing.test.ts
+++ packages/language/test/parsing.test.ts
@@ -1,60 +1,64 @@
-import { beforeAll, describe, expect, test } from "vitest";
-import { EmptyFileSystem, type LangiumDocument } from "langium";
-import { expandToString as s } from "langium/generate";
-import { parseHelper } from "langium/test";
-import type { OOCModel } from "object-oriented-c-language";
-import { createObjectOrientedCServices, isOOCModel } from "object-oriented-c-language";
+import { beforeAll, describe, expect, test } from 'vitest'
+import { EmptyFileSystem, type LangiumDocument } from 'langium'
+import { expandToString as s } from 'langium/generate'
+import { parseHelper } from 'langium/test'
+import type { OOCModel } from 'object-oriented-c-language'
+import {
+  createObjectOrientedCServices,
+  isOOCModel,
+} from 'object-oriented-c-language'
 
-let services: ReturnType<typeof createObjectOrientedCServices>;
-let parse:    ReturnType<typeof parseHelper<OOCModel>>;
-let document: LangiumDocument<OOCModel> | undefined;
+let services: ReturnType<typeof createObjectOrientedCServices>
+let parse: ReturnType<typeof parseHelper<OOCModel>>
+let document: LangiumDocument<OOCModel> | undefined
 
 beforeAll(async () => {
-    services = createObjectOrientedCServices(EmptyFileSystem);
-    parse = parseHelper<OOCModel>(services.ObjectOrientedC);
+  services = createObjectOrientedCServices(EmptyFileSystem)
+  parse = parseHelper<OOCModel>(services.ObjectOrientedC)
 
-    // activate the following if your linking test requires elements from a built-in library, for example
-    // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
-});
+  // activate the following if your linking test requires elements from a built-in library, for example
+  // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
+})
 
+/**
+ *
+ */
 describe('Parsing tests', () => {
+  test('parse simple OOCModel with bindings and objects', async () => {
+    document = await parse(`     
+            // Assignment
+            counter =: result ;
+            
+            // Pipe operations
+            'abcdef' slice 1 4 | startsWith 'b';
+              // Object definition
+            obj = {
+                (increment x) {
+                    x add 1
+                }
+                (greet name) {
+                    'Hello, ' contact name
+                }
+            };
+        `)
 
-    test('parse simple OOCModel', async () => {
-        document = await parse(`
-            person Langium
-            Hello Langium!
-        `);
-
-        // check for absence of parser errors the classic way:
-        //  deactivated, find a much more human readable way below!
-        // expect(document.parseResult.parserErrors).toHaveLength(0);
-
-        expect(
-            // here we use a (tagged) template expression to create a human readable representation
-            //  of the AST part we are interested in and that is to be compared to our expectation;
-            // prior to the tagged template expression we check for validity of the parsed document object
-            //  by means of the reusable function 'checkDocumentValid()' to sort out (critical) typos first;
-            checkDocumentValid(document) || s`
-                Persons:
-                  ${document.parseResult.value?.persons?.map(p => p.name)?.join('\n  ')}
-                Greetings to:
-                  ${document.parseResult.value?.greetings?.map(g => g.person.$refText)?.join('\n  ')}
-            `
-        ).toBe(s`
-            Persons:
-              Langium
-            Greetings to:
-              Langium
-        `);
-    });
-});
+    expect(checkDocumentValid(document)).toBeUndefined()
+  })
+})
 
 function checkDocumentValid(document: LangiumDocument): string | undefined {
-    return document.parseResult.parserErrors.length && s`
+  return (
+    (document.parseResult.parserErrors.length &&
+      s`
         Parser errors:
-          ${document.parseResult.parserErrors.map(e => e.message).join('\n  ')}
-    `
-        || document.parseResult.value === undefined && `ParseResult is 'undefined'.`
-        || !isOOCModel(document.parseResult.value) && `Root AST object is a ${document.parseResult.value.$type}, expected a 'OOCModel'.`
-        || undefined;
+          ${document.parseResult.parserErrors
+            .map((e) => e.message)
+            .join('\n  ')}
+    `) ||
+    (document.parseResult.value === undefined &&
+      `ParseResult is 'undefined'.`) ||
+    (!isOOCModel(document.parseResult.value) &&
+      `Root AST object is a ${document.parseResult.value.$type}, expected a 'OOCModel'.`) ||
+    undefined
+  )
 }
